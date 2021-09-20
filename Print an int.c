17fefd4c73bdc21d6d6ad887c588a216f1a494f3
Priyanka V <89244771+Priyankavad@users.noreply.github.com>
2021-09-20 17:37:34 +0530

    Create Print an int.c

---
+++ Print an int.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {   
+    int num;
+   
+    printf("Enter an integer: ");  
+    
+    // reads and stores input
+    scanf("%d", &num);
+
+    // displays output
+    printf("You entered: %d", num);
+    
+    return 0;
+}
