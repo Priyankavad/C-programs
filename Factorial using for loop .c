650d67f38bb39cd238ee57b32c6ddc7baf76d58b
Priyanka V <89244771+Priyankavad@users.noreply.github.com>
2021-09-27 15:17:51 +0530

    Update Factorial using for loop .c

--- Factorial using for loop .c
+++ Factorial using for loop .c
@@ -1,10 +1,10 @@
 #include<stdio.h>  
 int main()    
 {    
- int i,facto=55,number;    
+ int i,facto=1,number;    
  printf("Enter a number: ");    
   scanf("%d",&number);    
-    for(i=55;i<=number;i++){    
+    for(i=1;i<=number;i++){    
       facto=facto*i;    
   }    
   printf("Factorial of %d is: %d",number,facto);    
